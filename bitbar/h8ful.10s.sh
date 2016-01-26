83a159d1aa9680fef686e66ab1a6dd1bc013eef3
Adam Chainz <adam@adamj.eu>
2016-01-26 13:11:45 +0000

    no need to poll for hateful eight any more

--- bitbar/h8ful.10s.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-/usr/local/bin/http get 'http://www.odeon.co.uk/showtimes/day/?date=2016-01-30&siteId=105&filmMasterId=100876&container=DAY8&type=DAY' | grep 'not been programmed' >/dev/null && echo "wait" || echo "BUY H8FUL"
