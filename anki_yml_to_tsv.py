a2e4ffdfebd4fe476c9fd296c25f55b1fd9fa1ce
Adam Johnson <me@adamj.eu>
2018-03-24 15:21:52 +0000

    Make anki_yml_to_tsv python 3 compatible

--- anki_yml_to_tsv.py
+++ anki_yml_to_tsv.py
@@ -16,9 +16,8 @@ Your yaml should look like this:
 
 Run the script, then import the TSVs using the usual anki import file function.
 """
-from __future__ import unicode_literals
-
 from pathlib import Path
+
 import yaml
 
 
@@ -30,17 +29,17 @@ def main():
 
 
 def convert(yml_path):
-    print "Converting", yml_path
+    print("Converting", yml_path)
     text = yml_path.open('r', encoding='utf8').read()
 
     all_docs = [doc for doc in yaml.load_all(text)
                 if isinstance(doc, list) and len(doc)]
 
     if not len(all_docs):
-        print "\tEmpty."
+        print("\tEmpty.")
         return
 
-    print "\t", len(all_docs), "note[s]."
+    print("\t", len(all_docs), "note[s].")
     tsv_path = yml_path.with_suffix('.tsv')
     with tsv_path.open('w') as tsv:
         for doc in all_docs:
@@ -53,8 +52,8 @@ def lineify(doc):
 
     assert len(doc) == 2, doc
 
-    front = unicode(doc[0]).replace('\n', '<br>').strip()
-    back = unicode(doc[1]).replace('\n', '<br>').strip()
+    front = str(doc[0]).replace('\n', '<br>').strip()
+    back = str(doc[1]).replace('\n', '<br>').strip()
     return "{}\t{}\n".format(front, back)
 
 if __name__ == '__main__':
