7fa5cb4a25002dc6af30f6e3d98f20f6f67bbed7
Adam Johnson <me@adamj.eu>
2019-08-19 16:36:28 +0100

    Blackify archive_photos.py

--- archive_photos.py
+++ archive_photos.py
@@ -6,17 +6,17 @@ import subprocess
 
 from termcolor import colored
 
-iphone_dir = os.path.expanduser('~/Arqbox/Aart/Photos/iPhone')
-photo_dir = os.path.expanduser('~/Arqbox/Aart/Photos')
-trash = os.path.expanduser('~/.Trash')
+iphone_dir = os.path.expanduser("~/Arqbox/Aart/Photos/iPhone")
+photo_dir = os.path.expanduser("~/Arqbox/Aart/Photos")
+trash = os.path.expanduser("~/.Trash")
 
 
 def blue(string):
-    return colored(string, 'blue')
+    return colored(string, "blue")
 
 
 def red(string):
-    return colored(string, 'red')
+    return colored(string, "red")
 
 
 def main():
@@ -25,47 +25,32 @@ def main():
         print(filename)
         date_taken = get_date_taken(filename)
         if date_taken is None:
-            print(red('\t😓  Could not find date/time'))
+            print(red("\t😓  Could not find date/time"))
             continue
 
         if date_taken > thirty_days_ago:
-            print(blue('\tWas taken < 30 days ago, leaving'))
+            print(blue("\tWas taken < 30 days ago, leaving"))
             continue
 
         destination_dir = get_date_dir(date_taken)
-        print(blue('\tMoving to {}'.format(destination_dir)))
+        print(blue("\tMoving to {}".format(destination_dir)))
 
-        destination_filename = os.path.join(
-            destination_dir, os.path.basename(filename)
-        )
+        destination_filename = os.path.join(destination_dir, os.path.basename(filename))
         os.rename(filename, destination_filename)
 
 
-FILE_EXTENSIONS = (
-    '.avi',
-    '.jpg',
-    '.jpeg',
-    '.mov',
-    '.mp4',
-    '.png',
-)
+FILE_EXTENSIONS = (".avi", ".jpg", ".jpeg", ".mov", ".mp4", ".png")
 
 
 def old_media():
     for filename in os.listdir(iphone_dir):
-        if (
-            filename.lower().endswith(FILE_EXTENSIONS) and
-            not os.path.islink(filename)
-        ):
+        if filename.lower().endswith(FILE_EXTENSIONS) and not os.path.islink(filename):
             yield os.path.join(iphone_dir, filename)
 
 
 def get_date_taken(filename):
-    output = subprocess.check_output(
-        ['exiftool', filename],
-        universal_newlines=True,
-    )
-    lines = output.split('\n')
+    output = subprocess.check_output(["exiftool", filename], universal_newlines=True)
+    lines = output.split("\n")
 
     exif_date_taken = get_exif_date_taken(filename, lines)
     if exif_date_taken:
@@ -79,10 +64,10 @@ def get_date_taken(filename):
 
 
 def get_exif_date_taken(filename, lines):
-    if filename.lower().endswith('.mov'):
-        exif_field_names = ('Media Create Date', 'File Modification Date/Time')
+    if filename.lower().endswith(".mov"):
+        exif_field_names = ("Media Create Date", "File Modification Date/Time")
     else:
-        exif_field_names = ('Date/Time Original',)
+        exif_field_names = ("Date/Time Original",)
 
     lines = [l for l in lines if l.startswith(exif_field_names)]
     if not lines:
@@ -91,7 +76,7 @@ def get_exif_date_taken(filename, lines):
 
 
 def get_file_date_taken(filename, lines):
-    lines = [l for l in lines if 'File Modification Date/Time' in l]
+    lines = [l for l in lines if "File Modification Date/Time" in l]
     if not lines:
         return None
     return extract_datetime(lines[0])
@@ -104,7 +89,7 @@ def extract_datetime(line):
 
 
 date_taken_re = re.compile(
-    r'(?P<year>\d{4}):(?P<month>\d{2}):(?P<day>\d{2}) \d{2}:\d{2}:\d{2}'
+    r"(?P<year>\d{4}):(?P<month>\d{2}):(?P<day>\d{2}) \d{2}:\d{2}:\d{2}"
 )
 
 
@@ -124,5 +109,5 @@ def get_date_dir(date):
     return basic_path
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
