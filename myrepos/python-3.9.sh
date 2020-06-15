69ee0e11e7ec34019781baecb2265732a6fcda1b
Adam Johnson <me@adamj.eu>
2020-06-15 13:33:47 +0100

    Update python 3.9 myrepos script

--- myrepos/python-3.9.sh
+++ myrepos/python-3.9.sh
@@ -4,10 +4,10 @@ set -e
 git switch -c python_3.9
 sed -E -i '' -e 's/    Programming Language :: Python :: 3.8/    Programming Language :: Python :: 3.8\
     Programming Language :: Python :: 3.9/g' setup.cfg
-sed -E -i '' -e 's/  - python: 3.8/  - python: 3.8\
-  - python: 3.9-dev/g' .travis.yml
-subl -w setup.cfg requirements/compile.py tox.ini .travis.yml
+sed -E -i '' -e 's/        - 3.8/        - 3.8\
+        - 3.9/g' .github/workflows/main.yml
+subl -w .github/workflows/main.yml requirements/compile.py tox.ini setup.cfg
 ./requirements/compile.py
 git add --all
 git commit -m "Test with Python 3.9"
-pushupr
+hub pull-request --push --no-edit -o
