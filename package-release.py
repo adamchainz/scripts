3ecddce2eae017502e80a699c57065ca1adbf396
Adam Johnson <me@adamj.eu>
2021-09-25 10:14:55 +0100

    package-release.py check all GitHub Actions checks are successful

--- package-release.py
+++ package-release.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import argparse
 import datetime as dt
+import json
 import os
 import subprocess
 import sys
 from configparser import ConfigParser
 from functools import partial
 from glob import glob
+from textwrap import dedent
 
 from packaging.version import Version
 
@@ -44,6 +46,76 @@ def main(argv=None) -> int:
         )
         return 1
 
+    # Check GitHub Actions are all successful for the current commit
+    # May be replaceable in future with gh cli builtin:
+    # https://github.com/cli/cli/issues/1055
+    latest_commit = run(
+        ["git", "rev-parse", "HEAD"], capture_output=True, text=True
+    ).stdout.strip()
+    status_query = dedent(
+        """\
+        query ($owner: String!, $name: String!, $commit: String!) {
+          repository(owner: $owner, name: $name) {
+            object(expression: $commit) {
+              ... on Commit {
+                checkSuites(first: 100) {
+                  nodes {
+                    creator {
+                      name
+                    }
+                    resourcePath
+                    status
+                    app {
+                      name
+                    }
+                    conclusion
+                    checkRuns(first: 10) {
+                      nodes {
+                        conclusion
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        """
+    )
+    checks_data = json.loads(
+        run(
+            [
+                "gh",
+                "api",
+                "graphql",
+                "-F",
+                r"owner={owner}",
+                "-F",
+                r"name={repo}",
+                "-F",
+                f"commit={latest_commit}",
+                "-f",
+                f"query={status_query}",
+            ],
+            capture_output=True,
+            text=True,
+        ).stdout
+    )
+    check_suites = checks_data["data"]["repository"]["object"]["checkSuites"][
+        "nodes"
+    ]
+    check_suites = [s for s in check_suites if s["app"] is not None]
+    if not all(s["conclusion"] == "SUCCESS" for s in check_suites):
+        print("❌ Not all checks are successful:", file=sys.stderr)
+        for check_suite in check_suites:
+            for check_run in check_suite["checkRuns"]["nodes"]:
+                print(
+                    f"    {check_run['name']}: {check_run['conclusion']}",
+                    file=sys.stderr,
+                )
+        return 1
+
     run(
         ["sd", "version = .*", f"version = {version}", "setup.cfg"],
     )
@@ -71,7 +143,10 @@ def main(argv=None) -> int:
     if sdist_only:
         run(["python", "setup.py", "clean", "sdist"])
     else:
-        run(["python", "-m", "build"], env={**os.environ, "PIP_REQUIRE_VIRTUALENV": ""})
+        run(
+            ["python", "-m", "build"],
+            env={**os.environ, "PIP_REQUIRE_VIRTUALENV": ""},
+        )
 
     run(["twine", "check", *glob("dist/*")])
     run(["twine", "upload", *glob("dist/*")])
