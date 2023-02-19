db7640444c49ef123132f5e9eb7c264ee8900c36
Adam Johnson <me@adamj.eu>
2023-02-19 07:48:56 +0000

    archive_photos.py: rewrite with modern Python

--- archive_photos.py
+++ archive_photos.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+import argparse
 import datetime as dt
-import os
 import re
 import shutil
 import subprocess
-import time
+from pathlib import Path
 
-iphone_dir = os.path.expanduser("~/Arqbox/Aart/Photos/iPhone")
-photo_dir = os.path.expanduser("~/Arqbox/Aart/Photos")
-trash = os.path.expanduser("~/.Trash")
+iphone_path = Path("~/Arqbox/Aart/Photos/iPhone").expanduser()
+photos_path = Path("~/Arqbox/Aart/Photos").expanduser()
+trash_path = Path("~/.Trash").expanduser()
 
 esc_blue = "\x1b[38;2;100;100;255m"
 esc_red = "\x1b[38;2;255;100;100m"
@@ -23,86 +23,100 @@ def red(string):
     return f"{esc_red}{string}{esc_reset}"
 
 
-def main():
-    recent = dt.date.today() - dt.timedelta(days=180)
-    for filename in old_media():
-        print(filename)
-        date_taken = get_date_taken(filename)
+def main(argv=None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--actually-move", action="store_true")
+    args = parser.parse_args(argv)
+    actually_move = args.actually_move
+
+    # Keep recent photos in the iPhone directory
+    recent_cutoff = dt.date.today() - dt.timedelta(days=30)
+
+    for path in media():
+        print(blue(path.relative_to(iphone_path)), end=" ")
+
+        if path.suffix.lower() == ".png":
+            subprocess.run(
+                ["oxipng", str(path)], check=True, capture_output=True
+            )
+            print("✔", end=" ")
+        elif path.suffix.lower() in (".jpg", ".jpeg"):
+            subprocess.run(
+                ["jpegoptim", str(path)], check=True, capture_output=True
+            )
+            print("✔", end=" ")
+
+        date_taken = get_date_taken(path)
         if date_taken is None:
-            print(red("\t😓  Could not find date/time"))
+            print(red("😓  Could not find date/time"))
+            continue
+        print("@", date_taken, end=" ")
+
+        if date_taken >= recent_cutoff:
+            print(blue("recent, leaving in place."))
             continue
 
-        if date_taken >= recent:
-            print(blue("\tWas taken within recent window, leaving"))
-            continue
+        destination_path = get_date_path(date_taken) / path.name
+        dest_display = blue(destination_path.relative_to(photos_path).parent)
+        if not actually_move:
+            print("would move to", dest_display)
 
-        destination_dir = get_date_dir(date_taken)
-        print(blue("\tMoving to {}".format(destination_dir)))
-
-        destination_filename = os.path.join(
-            destination_dir, os.path.basename(filename)
-        )
-        os.rename(filename, destination_filename)
-
-        if filename.lower().endswith((".png")):
-            subprocess.run(["oxipng", destination_filename], check=True)
-        elif filename.lower().endswith((".jpg", ".jpeg")):
-            iphone_aae_filename = filename.rsplit(".", 1)[0] + ".aae"
-            if os.path.isfile(iphone_aae_filename):
-                print(blue("\tTrashing related iPhone photo edits AAE file"))
-                shutil.move(iphone_aae_filename, trash)
-
-            subprocess.run(["jpegoptim", destination_filename], check=True)
-
-        time.sleep(1)
+        if actually_move:
+            print("->", dest_display)
+            shutil.move(path, destination_path)
+            if path.suffix.lower() in (".jpg", ".jpeg"):
+                iphone_aae_path = path.with_suffix(".aae")
+                if iphone_aae_path.exists():
+                    print(blue("\t🗑️ iPhone photo edits AAE file"))
+                    shutil.move(iphone_aae_path, trash_path)
 
 
-FILE_EXTENSIONS = (".avi", ".jpg", ".jpeg", ".mov", ".mp4", ".png")
+FILE_EXTENSIONS = {".avi", ".jpg", ".jpeg", ".mov", ".mp4", ".png"}
 
 
-def old_media():
-    for filename in sorted(os.listdir(iphone_dir)):
-        if filename.lower().endswith(FILE_EXTENSIONS) and not os.path.islink(
-            filename
+def media():
+    paths = list(iphone_path.iterdir())
+    paths.sort()  # Basically random order thanks to random filenames.
+    for path in paths:
+        if (
+            path.suffix.lower() in FILE_EXTENSIONS
+            and not path.is_symlink()
+            and path.exists()
         ):
-            full_filename = os.path.join(iphone_dir, filename)
-            # mov files can be removed during the loop if they are iPhone live
-            # photo movies
-            if os.path.exists(full_filename):
-                yield full_filename
+            yield path
 
 
-def get_date_taken(filename):
-    output = subprocess.check_output(
-        ["exiftool", filename], universal_newlines=True
-    )
+def get_date_taken(path):
+    output = subprocess.run(
+        ["exiftool", str(path)], capture_output=True, text=True, check=True
+    ).stdout
     lines = output.split("\n")
 
-    exif_date_taken = get_exif_date_taken(filename, lines)
+    exif_date_taken = get_exif_date_taken(path, lines)
     if exif_date_taken:
         return exif_date_taken
 
-    file_date_taken = get_file_date_taken(filename, lines)
+    file_date_taken = get_file_date_taken(lines)
     if file_date_taken:
         return file_date_taken
 
     return None
 
 
-def get_exif_date_taken(filename, lines):
-    if filename.lower().endswith(".mov"):
+def get_exif_date_taken(path, lines):
+    if path.suffix.lower() == ".mov":
         exif_field_names = ("Media Create Date", "File Modification Date/Time")
     else:
         exif_field_names = ("Date/Time Original",)
 
-    lines = [l for l in lines if l.startswith(exif_field_names)]
+    lines = [line for line in lines if line.startswith(exif_field_names)]
     if not lines:
         return None
-    return min(extract_datetime(l) for l in lines)
+    return min(extract_datetime(line) for line in lines)
 
 
-def get_file_date_taken(filename, lines):
-    lines = [l for l in lines if "File Modification Date/Time" in l]
+def get_file_date_taken(lines):
+    lines = [line for line in lines if "File Modification Date/Time" in line]
     if not lines:
         return None
     return extract_datetime(lines[0])
@@ -119,21 +133,21 @@ date_taken_re = re.compile(
 )
 
 
-def get_date_dir(date):
-    year_folder = os.path.join(photo_dir, str(date.year))
-    os.makedirs(year_folder, exist_ok=True)  # Just in case it's a new year
+def get_date_path(date):
+    year_path = photos_path / str(date.year)
+    year_path.mkdir(parents=True, exist_ok=True)  # new year
 
     # Might exist already as date_string + textual name
     date_string = date.isoformat()
-    for name in os.listdir(year_folder):
-        if name.startswith(date_string):
-            return os.path.join(year_folder, name)
+    for path in year_path.iterdir():
+        if path.parts[-1].startswith(date_string):
+            return path
 
     # Does not exist, create
-    basic_path = os.path.join(year_folder, date_string)
-    os.makedirs(basic_path, exist_ok=True)
-    return basic_path
+    path = year_path / date_string
+    path.mkdir()
+    return path
 
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
