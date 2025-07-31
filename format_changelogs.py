fb8a3cb4ddf9b72c389526e90c66c490fed2697e
Adam Johnson <me@adamj.eu>
2025-07-31 09:58:34 +0100

    format_changelogs.py: support uv.lock diffs, add more packages

--- format_changelogs.py
+++ format_changelogs.py
@@ -20,35 +20,35 @@ def main():
     out = []
     fail = False
 
-    for line in sys.stdin.readlines():
-        if not line.startswith("+") or "==" not in line:
-            continue
-
-        pkg, *_ = line.lstrip("+").split("==")
-
-        link = links.get(pkg)
-        if link is None:
-            # Use urllib3 to request JSON from PyPI and find the changelog link there
-            response = urllib3.request(
-                "GET", "https://pypi.python.org/pypi/{pkg}/json".format(pkg=pkg)
-            )
-            if response.status != 200:
-                print("{pkg} not found on PyPI".format(pkg=pkg), file=sys.stderr)
-                fail = True
+    input = sys.stdin.read()
+    if "--- ./uv.lock" in input:
+        # uv.lock diff
+        pkg = None
+        for line in input.splitlines():
+            if not line.startswith(' name = "'):
                 continue
-            data = response.json()
-            project_urls = data["info"]["project_urls"]
-            if "Changelog" in project_urls:
-                link = project_urls["Changelog"]
-            else:
-                rprint(
-                    f"[link=https://pypi.org/project/{pkg}]{pkg}[/link] changelog link unknown",
-                    file=sys.stderr,
-                )
+
+            pkg = line.split('"')[1]
+            link = get_changelog_link(pkg)
+            if link is None:
                 fail = True
                 continue
 
-        out.append(f"* [{pkg}]({link}) - ")
+            out.append(f"* [{pkg}]({link}) - ")
+    else:
+        # requirements.txt diff
+        for line in input.splitlines():
+            if not line.startswith("+") or "==" not in line:
+                continue
+
+            pkg, *_ = line.lstrip("+").split("==")
+
+            link = get_changelog_link(pkg)
+            if link is None:
+                fail = True
+                continue
+
+            out.append(f"* [{pkg}]({link}) - ")
 
     if fail:
         raise SystemExit(1)
@@ -59,29 +59,57 @@ def main():
             print(line)
 
 
+def get_changelog_link(pkg: str) -> str | None:
+    link = links.get(pkg.lower())
+    if link is None:
+        # Use urllib3 to request JSON from PyPI and find the changelog link there
+        response = urllib3.request(
+            "GET", "https://pypi.python.org/pypi/{pkg}/json".format(pkg=pkg)
+        )
+        if response.status != 200:
+            print("{pkg} not found on PyPI".format(pkg=pkg), file=sys.stderr)
+            return None
+        data = response.json()
+        project_urls = data["info"]["project_urls"]
+        if "Changelog" in project_urls:
+            link = project_urls["Changelog"]
+        else:
+            rprint(
+                f"[link=https://pypi.org/project/{pkg}]{pkg}[/link] changelog link unknown",
+                file=sys.stderr,
+            )
+            return None
+    return link
+
+
 links = {
     "billiard": "https://pypi.python.org/pypi/billiard",
     "boto": "http://docs.pythonboto.org/en/latest/#release-notes",
     "boto3": "https://github.com/boto/boto3/blob/develop/CHANGELOG.rst",
     "botocore": "https://github.com/boto/botocore/blob/develop/CHANGELOG.rst",
     "celery": "http://docs.celeryproject.org/en/latest/changelog.html",
+    "dj-database-url": "https://github.com/jazzband/dj-database-url/blob/master/CHANGELOG.md",
     "django-filter": "https://github.com/carltongibson/django-filter/blob/master/CHANGES.rst",
     "django-htmlmin": "https://github.com/cobrateam/django-htmlmin/commits/master",
+    "django-leaflet": "https://github.com/makinacorpus/django-leaflet/blob/master/CHANGES",
     "django-modeldict-yplan": "https://pypi.python.org/pypi/django-modeldict-yplan",
     "django-perf-rec": "https://pypi.python.org/pypi/django-perf-rec",
+    "django-picklefield": "https://github.com/gintas/django-picklefield#changes",
     "django-q2": "https://github.com/django-q2/django-q2/blob/master/CHANGELOG.md",
     "django-recaptcha": "https://pypi.python.org/pypi/django-recaptcha#changelog",
     "django-reversion": "https://github.com/etianen/django-reversion/blob/master/CHANGELOG.rst",
+    "django-schema-viewer": "https://github.com/pikhovkin/django-schema-viewer/commits/main/",
+    "djangorestframework-gis": "https://github.com/openwisp/django-rest-framework-gis/blob/master/CHANGES.rst",
     "flake8": "https://flake8.readthedocs.io/en/latest/release-notes/3.2.1.html",
     "flake8-coding": "https://github.com/tk0miya/flake8-coding/blob/master/CHANGES.rst",
     "flake8-tidy-imports": "https://pypi.python.org/pypi/flake8-tidy-imports",
     "fonttools": "https://github.com/fonttools/fonttools/blob/main/NEWS.rst",
     "jinja2": "https://jinja.palletsprojects.com/en/stable/changes/",
     "kombu": "https://kombu.readthedocs.io/en/latest/changelog.html",
-    "Markdown": "https://github.com/waylan/Python-Markdown/blob/master/docs/change_log.txt",
+    "markdown": "https://github.com/waylan/Python-Markdown/blob/master/docs/change_log.txt",
     "matplotlib": "https://matplotlib.org/stable/users/release_notes",
     "mycli": "https://github.com/dbcli/mycli/blob/master/changelog.md",
-    "Pillow": "https://github.com/python-pillow/Pillow/blob/master/CHANGES.rst",
+    "pillow": "https://github.com/python-pillow/Pillow/blob/master/CHANGES.rst",
     "pipdeptree": "https://github.com/naiquevin/pipdeptree/blob/master/CHANGES.md",
     "prompt-toolkit": "https://github.com/jonathanslenders/python-prompt-toolkit/blob/master/CHANGELOG",
     "pycodestyle": "https://pypi.python.org/pypi/pycodestyle",
@@ -103,7 +131,7 @@ links = {
     "ua-parser": "https://github.com/ua-parser/uap-python/commits/master",
     "vcrpy": "https://vcrpy.readthedocs.io/en/latest/changelog.html",
     "waitress": "https://github.com/Pylons/waitress/blob/master/CHANGES.txt",
-    "WebOb": "https://webob.readthedocs.io/en/stable/changes.html",
+    "webob": "https://webob.readthedocs.io/en/stable/changes.html",
 }
 
 
